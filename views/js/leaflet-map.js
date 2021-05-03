a29eb265c921803c1888f5d6f04fb569274b5362
Jon Legasa <jon.legasa@gmail.com>
2021-05-03 17:26:04 -0400

    Updated Files 5/3/2021 5:25 P<

--- views/js/leaflet-map.js
+++ views/js/leaflet-map.js
@@ -1,4 +1,5 @@
-URL = 'https://www.vaccinespotter.org/api/v0/states/NY.json';
+//assign
+
 
 var start = [42.5, -75];
 var setmarker = [0, 0];
@@ -8,19 +9,12 @@ var settings = {
     zoom: 7.5
 }
 
-const mymap = L.map('mapid', settings);
 
+const mymap = L.map('mapid', settings);
 
 let chosenmarkers = [];
 let circlesarray = [];
 
-const button = document.getElementById('clear');
-let deleteBool = false;
-
-
-var scrolltop = document.getElementById('top');
-
-let radius = document.getElementById('radius');
 
 
 var blueicon = new L.Icon({
@@ -50,7 +44,6 @@ var blackicon = new L.Icon({
     shadowSize: [41, 41]
 });
 
-
 function chooseAddr(lat1, lng1) {
      var chosenmarkers = L.marker(setmarker, { icon: blackicon}, { title: "Your Marker" }).addTo(mymap);
      mymap.setView([lat1, lng1], 14);
@@ -59,11 +52,8 @@ function chooseAddr(lat1, lng1) {
      lon = lng1.toFixed(8);
      chosenmarkers.bindPopup("Your Marker").openPopup();
 
-     function getMiles(i) {
-          return i*0.000621371192;
-     }
      function getMeters(i) {
-          return i*1609.344;
+         return i * 1609.344;
      }
 
      var circlesarray = L.circle([lat1, lng1], {
@@ -73,11 +63,16 @@ function chooseAddr(lat1, lng1) {
          radius: getMeters(1)
      }).addTo(mymap);
 
+     const button = document.getElementById('clear');
+     let deleteBool = false;
+
+
      button.addEventListener('click',()=>{
          let deleteBool = true;
          chosenmarkers.removeFrom(mymap);
          circlesarray.removeFrom(mymap);
-     })
+     });
+
 }
 
 
@@ -87,11 +82,10 @@ function myFunction(arr){
 
      if(arr.length > 0){
          for(i = 0; i < arr.length; i++){
-             // out += "<div class='address' title='Show Location and Coordinates' onclick='chooseAddr(" + arr[i].lat + ", " + arr[i].lon + ");return false;'><br />" + arr[i].display_name + "</div>";
              out += "<div class='address' title='Show Location and Coordinates' onclick='chooseAddr(" + arr[i].lat + ", " + arr[i].lon + ");return false;'>" + arr[i].display_name + "</div><br />";
          }
          document.getElementById('addressresult').innerHTML = out;
-        }else{
+     } else{
       document.getElementById('addressresult').innerHTML = "Sorry, no results...";
      }
 }
@@ -110,7 +104,17 @@ xmlhttp.open("GET", url, true);
 xmlhttp.send();
 }
 
-fetch(URL)
+//enter
+
+var input = document.getElementById("addr");
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("test").click();
+    }
+});
+
+fetch('https://www.vaccinesnearyou.com/api')
     .then(res => res.json())
     .then(res => {
         var data = {
@@ -118,7 +122,6 @@ fetch(URL)
                 error: res.error || null,
                 loading: false
         };
-
 
         var main_array = [];
         var url_array = [];
@@ -128,7 +131,6 @@ fetch(URL)
         var provider_array = [];
         var address_array = [];
         var coords_array = [];
-
 
         for (i = 0; i < data.data['features'].length; i++) {
             main_array.push(data.data['features'][i]);
@@ -152,37 +154,15 @@ fetch(URL)
             url            : url_array
         }
 
-
-        function filter_array(test_array) {
-            let index = -1;
-            const arr_length = test_array ? test_array.length : 0;
-            let resIndex = -1;
-            const result = [];
-
-            while (++index < arr_length) {
-                const value = test_array[index];
-
-                if (value) {
-                    result[++resIndex] = value;
-                }
-            }
-
-            return result;
-        }
-
-        console.log(filter_array([NaN, 0, 15, false, -22, '',undefined, 47, null]));
-
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
                     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 }).addTo(mymap);
 
 
-        console.log(dataset.typeofvaccines);
-
-
         var markers = L.markerClusterGroup();
         for (var i = 0; i < dataset.main.length; i++) {
             //assign variables
+            var marker;
             var marker_coordinates = dataset.coordinates[i];
             var vaccinesoutput;
             var appoinments;
@@ -191,7 +171,25 @@ fetch(URL)
             var title = '<div class="pop-container"><h3>Provider: ' + dataset.provider[i] + '</h3><h3>Appointments: ' + appoinments + '</h3><h3>Vaccines: ' + vaccinesoutput + '</h3><h3>Address: ' + address + '</h3><a href=' + dataset.url[i] + '><h3>Website: ' + dataset.url[i] + '</h3></a><h3>Vaccine Types: ' + vaccinetypes + '</h3></div>';
 
 
-            //assign marker color based on carry vaccines and appointments appointments available
+
+            //assign address
+            if (dataset.address[i] == null) {
+                address = "Unknown";
+            } else {
+                address = dataset.address[i];
+            }
+
+
+            if (dataset.typeofvaccines[i] == null) {
+                vaccinetypes = 'None';
+            } else if (dataset.typeofvaccines[i]['moderna'] == true) {
+                vaccinetypes = "Moderna";
+            } else if (dataset.typeofvaccines[i]['pfizer'] == true) {
+                vaccinetypes = "Pfizer";
+            } else if (dataset.typeofvaccines[i]['pfizer'] == true && dataset.typeofvaccines[i]['moderna'] == true) {
+                vaccinetypes = "Pfizer, Moderna";
+            }
+
             if(dataset.appoinments[i] == true && dataset.carryvaccines[i] == true) {
                 appoinments = "Yes";
                 vaccinesoutput = "Yes"
@@ -202,33 +200,6 @@ fetch(URL)
                 vaccinesoutput = "Yes"
                 var marker = L.marker(new L.LatLng(marker_coordinates[1], marker_coordinates[0]), { icon: redicon}, { title: title });
             }
-
-            //assign address
-            if (dataset.address[i] == null) {
-                address = "Unknown";
-            } else {
-                address = dataset.address[i];
-            }
-
-            //assign type of vaccines
-
-
-
-            //
-            // else if (dataset.typeofvaccines[i]['moderna'] == true) {
-            //     vaccinetypes = "Moderna";
-            // }
-
-
-            // else if (dataset.typeofvaccines[i]['pfizer'] == true) {
-            //     vaccinetypes = "Pfizer";
-            // }
-
-            // else if (dataset.typeofvaccines[i]['moderna'] == true) {
-            //     vaccinetypes = "Moderna";
-            // } else if (dataset.typeofvaccines[i]['pfizer'] == true) {
-            //     vaccinetypes = "Pfizer";
-            // }
 
             marker.bindPopup(title, {
                 maxWidth: "auto"
