7d698ca6776f17c697cb80a695145180a575bd69
Jon Legasa <jon.legasa@gmail.com>
2021-05-07 11:53:02 -0400

    Created php contact.html

--- views/pages/email.php
+++ views/pages/email.php
@@ -1,5 +1,11 @@
 <?php
 
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $recepient = "vaccinesnearyou@gmail.com";
+
+    mail($recepient,"My subject",$message);
 
 
-?>
+ ?>
