f506a50b8549fabfe5edd147e7cb5a3382bc73aa
Jon Legasa <jon.legasa@gmail.com>
2021-05-07 15:47:55 +0000

    Delete EMAIL.PHP contents

--- views/pages/email.php
+++ views/pages/email.php
@@ -1,15 +1,5 @@
 <?php
 
-if(isset($_POST['submit'])) {
-    $recipient = 'vaccinesnearyou@gmail.com';
-    // $subject=$_POST['sendersubject'];
-    $sender = $_POST['name'];
-    $senderEmail = $_POST['useremail'];
-    $message = $_POST['message'];
 
-    //$mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
-
-    mail($recipient, $mailBody, "From: $sender <$senderEmail>");
-}
 
 ?>
