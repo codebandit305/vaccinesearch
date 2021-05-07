2d42b2bea57941985737e654910eb9a8c69343d7
Jon Legasa <jon.legasa@gmail.com>
2021-05-07 11:33:19 -0400

    TEST Contact.html php

--- views/pages/email.php
+++ views/pages/email.php
@@ -1,11 +1,11 @@
 <?php
 
 if(isset($_POST['submit'])) {
-    $recipient='vaccinesnearyou@gmail.com';
+    $recipient = 'vaccinesnearyou@gmail.com';
     // $subject=$_POST['sendersubject'];
-    $sender=$_POST['name'];
-    $senderEmail=$_POST['useremail'];
-    $message=$_POST['message'];
+    $sender = $_POST['name'];
+    $senderEmail = $_POST['useremail'];
+    $message = $_POST['message'];
 
     //$mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
 
